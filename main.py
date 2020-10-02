1a672b4642e5b0b6a957c1c6f766b99e67794d31
Katerina Solstad (katya sorok) <katya.solstad@gmail.com>
2020-10-02 12:46:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+'''
+    Your pizza cafe makes pizzas in size of 6 slices.
+    write a program that greets the user from your cafe and asks
+    how many people will be in their party.
+    when you get the answer, you should tell the user how many pizzas
+    they have to order.           
+'''
